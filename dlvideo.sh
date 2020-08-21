b64342874e215708f76d1555a8e1a3167da4626f
Philip Nelson <philip@Philips-MacBook-Pro.local>
2020-08-21 16:38:35 -0500

    capture all links

--- dlvideo.sh
+++ dlvideo.sh
@@ -6,9 +6,18 @@ echo $WVIDEOID
 EMBEDURL="https://fast.wistia.net/embed/iframe/"${WVIDEOID}"?videoFoam=true"
 echo $EMBEDURL
 JSON=`curl $EMBEDURL | grep -e 'iframeInit.' | sed -e 's/W.iframeInit(//' | sed -e 's/);//'`
-echo 'JSON'
-DLURL=`echo $JSON | jq -r '.assets[] | select(.display_name=="250p").url'`
-echo $DLURL
-curl -O $DLURL
-mv *.bin $FILENAME.mp4
+#echo $JSON
+ORIGINALDLURL=`echo $JSON | jq -r '.assets[] | select(.display_name=="Original file").url'`
+A360PDLURL=`echo $JSON | jq -r '.assets[] | select(.display_name=="360p").url'`
+A250PDLURL=`echo $JSON | jq -r '.assets[] | select(.display_name=="250p").url'`
+A540PDLURL=`echo $JSON | jq -r '.assets[] | select(.display_name=="540p").url'`
+A720PDLURL=`echo $JSON | jq -r '.assets[] | select(.display_name=="720p").url'`
+echo "Original File link: ${ORIGINALDLURL}" >> ${FILENAME}_dl_urls.txt
+echo "360p link: ${A360PDLURL}" >> ${FILENAME}_dl_urls.txt
+echo "250p link: ${A250PDLURL}" >> ${FILENAME}_dl_urls.txt
+echo "540p link: ${A540PDLURL}" >> ${FILENAME}_dl_urls.txt
+echo "720p link: ${A720PDLURL}" >> ${FILENAME}_dl_urls.txt
+curl -O $A720PDLURL
+OUTPATH="./${FILENAME}.mp4"
+mv *.bin $OUTPATH
 echo "Successfully created file ${OUTPATH}"
